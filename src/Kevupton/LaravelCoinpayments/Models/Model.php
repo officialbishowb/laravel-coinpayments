2a6d19291a539bb70245ef723f8ef8e0e3285ab7
Bishow B <55999477+officialbishowb@users.noreply.github.com>
2024-10-19 20:38:31 +0200

    getTable method added to avoid prefix problem

--- src/Kevupton/LaravelCoinpayments/Models/Model.php
+++ src/Kevupton/LaravelCoinpayments/Models/Model.php
@@ -13,5 +13,19 @@ class Model extends EloquentModel
         $this->table = cp_table_prefix() . self::getTable(); #Fix multiple prefix issue
         parent::__construct($attr);
     }
-     # Removed getTable method to fix multiple prefix issue
+    
+      /**
+     * @return string
+     */
+    public function getTable()
+    {
+        $table = parent::getTable();
+
+        // Check if the prefix is already added before adding it again
+        if (strpos($table, cp_table_prefix()) === 0) {
+            return $table;
+        }
+
+        return cp_table_prefix() . parent::getTable();
+    }
 }
